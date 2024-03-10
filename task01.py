47d41a478aa04d3b7355c6476a36a007fc77b9da
Crystweb <remont.moy-pc@yandex.ru>
2024-03-10 15:40:59 +0200

    task01 - del comments

--- task01.py
+++ task01.py
@@ -1,33 +1,3 @@
-"""
-Завдання 1
-На першому етапі вам потрібно реалізувати функцію для виведення списку колег, яких потрібно привітати з днем народження 
-на тижні.
-У вас є список словників users, кожен словник у ньому обов'язково має ключі name та birthday. Така структура 
-представляє модель списку користувачів з їх іменами та днями народження. Де name — це рядок з ім'ям користувача,
-а birthday — це datetime об'єкт, в якому записаний день народження.
-"""
-#  from faker import Faker
-#  fake = Faker(locale="uk_UA")
-# # Кількість записів
-# num_records = 10
-
-# # Створення списку 
-# list_of_dicts = []
-
-# for _ in range(num_records):
-#     birthday = fake.date_of_birth(minimum_age=18, maximum_age=90)
-#     birthday = birthday.replace(month=3)
-#     person_dict = {
-#         "name": fake.name(),
-#         "birthday": fake.date_of_birth(minimum_age=18, maximum_age=90).strftime("%d.%m.%Y"),
-##         "birthday": birthday.strftime("%d.%m.%Y"),
-#         "phone": fake.phone_number()
-#     }
-#     list_of_dicts.append(person_dict)
-
-# print(list_of_dicts)
-
-
 from datetime import datetime, timedelta
 
 users = [
